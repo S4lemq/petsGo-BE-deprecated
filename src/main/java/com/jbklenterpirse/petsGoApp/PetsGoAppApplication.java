6f9c70c5b7c0c779f917c01acc2495a38ec76b5f
Peter Lubowicz <peter.lubowicz@gmail.com>
2022-04-25 10:44:57 +0200

    initial commit

---
+++ src/main/java/com/jbklenterpirse/petsGoApp/PetsGoAppApplication.java
@@ -0,0 +1,18 @@
+package com.jbklenterpirse.petsGoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {
+		SecurityAutoConfiguration.class,
+		DataSourceAutoConfiguration.class
+})
+public class PetsGoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsGoAppApplication.class, args);
+	}
+
+}
