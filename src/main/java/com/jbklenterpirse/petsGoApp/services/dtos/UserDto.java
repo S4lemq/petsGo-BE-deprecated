8afb8c8a3716640426416bddf2bc0964ce759802
Peter Lubowicz <peter.lubowicz@gmail.com>
2022-05-07 11:57:58 +0200

    add user and role entiy, repository and dto

---
+++ src/main/java/com/jbklenterpirse/petsGoApp/services/dtos/UserDto.java
@@ -0,0 +1,65 @@
+package com.jbklenterpirse.petsGoApp.services.dtos;
+
+import java.util.Objects;
+
+public class UserDto {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserDto userDto = (UserDto) o;
+        return Objects.equals(username, userDto.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username);
+    }
+
+    @Override
+    public String toString() {
+        return "UserDto{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
